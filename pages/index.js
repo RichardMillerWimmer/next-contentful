aaa9d7787ded5aa4aa963457f1409b3e50aebf7f
Richard Miller <richardmmill@gmail.com>
2021-10-08 14:17:38 -0400

    app connected to Contentful

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,24 @@
-export default function Home() {
+import { createClient } from "contentful"
+
+export default function Home({recipes}) {
+
+  console.log(recipes)
+
   return (
     <div className='recipe-list'>
       
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const client = createClient({ space: process.env.CONTENTFUL_SPACE_ID, accessToken: process.env.CONTENTFUL_ACCESS_KEY })
+
+  const res = await client.getEntries({content_type: 'recipe'})
+
+  return {
+    props: {
+      recipes: res.items
+    }
+  }
 }
