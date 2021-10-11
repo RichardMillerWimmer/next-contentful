fa8aac9d1ce62bcb9e1f7aac38f32016f5d50e06
Richard Miller <richardmmill@gmail.com>
2021-10-11 16:44:39 -0400

    jsx styling

--- components/RecipeCard.js
+++ components/RecipeCard.js
@@ -4,8 +4,6 @@ import Image from 'next/image';
 
 const RecipeCard = ({recipe}) => {
     const { title, slug, cookingTime, thumbnail } = recipe.fields;
-
-    console.log(thumbnail)
      
     return (
         <div className='card'>
@@ -17,10 +15,45 @@ const RecipeCard = ({recipe}) => {
                     <h4>{ title }</h4>
                     <p>Takes Appox. { cookingTime } minutes to make</p>
                 </div>
-                <div className='action'>
-                    <Link href={`/recipes/${slug}`}>Cook This</Link>
+                <div className='actions'>
+                    <Link href={`/recipes/${slug}`}><a>Cook This</a></Link>
                 </div>
             </div>
+            <style jsx>{`
+        .card {
+          transform: rotateZ(-1deg);
+        }
+        .content {
+          background: #fff;
+          box-shadow: 1px 3px 5px rgba(0,0,0,0.1);
+          margin: 0;
+          position: relative;
+          top: -40px;
+          left: -10px;
+        }
+        .info {
+          padding: 16px;
+        }
+        .info h4 {
+          margin: 4px 0;
+          text-transform: uppercase;
+        }
+        .info p {
+          margin: 0;
+          color: #777;
+        }
+        .actions {
+          margin-top: 20px;
+          display: flex;
+          justify-content: flex-end;
+        }
+        .actions a {
+          color: #fff;
+          background: #f01b29;
+          padding: 16px 24px;
+          text-decoration: none;
+        }
+      `}</style>
         </div>
     )
 }
